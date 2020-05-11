7098750ee50a0ed19a7fc858b6abde18c5526bbd
Bharath-Radhakrishnan <Bharath.Radhakrishnan16@gmail.com>
2020-05-11 14:58:23 +0530

    added image and generate random image on button press

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -15,7 +17,32 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
       ),
       backgroundColor: Colors.blue,
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(4) + 1;
+              print('I got Clicked $ballNumber');
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
     );
   }
 }
